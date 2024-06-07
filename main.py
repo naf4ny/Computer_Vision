bc558165bdd644ac0b23f8fe434252f2c9b2b412
naf4ny <124737139+naf4ny@users.noreply.github.com>
2024-06-07 13:47:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+import time
+import os
+
+# Connecting the camera
+cap = cv2.VideoCapture(0)
+cap.set(50, 1050)
+cap.set(60, 1080)
+cap.set(10, 100)
+
+mp_drawing = mp.solutions.drawing_utils
+mp_holistic = mp.solutions.holistic
+
+pTime = 0
+cTime = 0
+
+# Receiving frames from the camera
+while True:
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+        ret, frame = cap.read()
+
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        results = holistic.process(image)
+
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
+                                  mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
+                                  mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
+                                  )
+
+        # Right Hand
+        mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+
+        # Left Hand
+        mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+
+        # Pose Detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(image, str(int(fps)), (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+
+        cv2.imshow('python', image)
+
+    if cv2.waitKey(1) == 27:  # exit on ESС
+        break
+
+cv2.destroyWindow("python")
+cap.release()
+cv2.waitKey(1)
